d8a89aa402343885b2276eff092908090770a784
Adriano Ferreira <a.r.ferreira@gmail.com>
2017-08-13 13:05:03 -0700

    Implement "allow_bignum" and "as_unblessed" with fallbacks
    Also introduce the "remove_fallback" method.

--- lib/JSON/PP/Monkey.pm
+++ lib/JSON/PP/Monkey.pm
@@ -8,12 +8,71 @@ use warnings;
 use parent qw(JSON::PP);
 
 use Carp ();
-use Scalar::Util qw(blessed refaddr);
+use Scalar::Util qw(blessed refaddr reftype);
 
 sub add_fallback {
     my ($self, $case, $cb) = @_;
-    push @{$self->{fallbacks}{$case}}, $cb and return $self;
+    push @{$self->{fallbacks}{$case}}, $cb;
+    return $self;
 }
+
+sub remove_fallback {
+    my ($self, $case, $cb) = @_;
+
+    if ($cb) {
+        $self->{fallbacks}{$case} = [grep { $cb ne $_ } @{$self->{fallbacks}{$case}}];
+        delete $self->{fallbacks}{$case} unless @{$self->{fallbacks}{$case}};
+    }
+
+    return $self;
+}
+
+# Helpful fallbacks
+
+sub convert_bignum {
+    return unless _is_bignum($_[1]);
+    return "$_[1]";
+}
+
+sub convert_as_nonblessed {    # Based on JSON::PP::blessed_to_json
+    my $obj = $_[1];
+    my $reftype = reftype($obj) || '';
+    if ($reftype eq 'HASH') {
+        return {%$obj};
+    }
+    elsif ($reftype eq 'ARRAY') {
+        return [@$obj];
+    }
+    else {
+        return undef;
+    }
+}
+
+# Reimplementation of 'allow_bignum' and 'as_nonblessed'
+
+sub allow_bignum {
+    my $self = shift->SUPER::allow_bignum(@_);
+    if ($self->get_allow_bignum) {
+        $self->add_fallback('blessed', 'convert_bignum');
+    }
+    else {
+        $self->remove_fallback('blessed','convert_bignum');
+    }
+    return $self;
+}
+
+sub as_nonblessed {
+    my $self = shift->SUPER::as_nonblessed(@_);
+    if ($self->get_as_nonblessed) {
+        $self->add_fallback('blessed', 'convert_as_nonblessed');
+    }
+    else {
+        $self->remove_fallback('blessed','convert_as_nonblessed');
+    }
+    return $self;
+}
+
+###
 
 {
     sub object_to_json {
@@ -46,9 +105,6 @@ sub add_fallback {
                     return $self->object_to_json( $result );
                 }
 
-                my $bignum = $self->get_allow_bignum;
-                return "$obj" if ( $bignum and _is_bignum($obj) );
-
                 my $allow_blessed = $self->get_allow_blessed;
                 if ($allow_blessed) {
                     if (my $s = $self->{fallbacks}{blessed}) {
@@ -68,8 +124,6 @@ sub add_fallback {
                             }
                         }
                     }
-                    my $as_nonblessed = $self->get_as_nonblessed;
-                    return $self->blessed_to_json($obj) if ($as_nonblessed); # will be removed.
                     return 'null';
                 }
                 encode_error( sprintf("encountered object '%s', but neither allow_blessed "
@@ -110,6 +164,22 @@ The primary reason it has been created was to allow
 dumping arbitrary Perl data into JSON – see the upcoming
 L<Shell::Perl::Dumper::JSON>.
 
+=head1 CAVEATS
+
+Notice that the order of fallbacks is important:
+
+    $json = JSON::PP->new->utf8->allow_blessed->allow_bignum->as_unblessed;
+
+works as expected – bignums converted to their stringified values
+and other blessed objects encoded as their underlying data structure.
+But
+
+    $json = JSON::PP->new->utf8->allow_blessed->as_unblessed->allow_bignum;
+
+will convert all blessed objects (including bignums) into their underlying
+data structures. That happens because the "as_unblessed" fallback
+is a catch-all fallback, instead of being selective as the "allow_bignum" fallback.
+
 =head1 METHODS
 
 all of JSON::PP and plus
@@ -119,6 +189,12 @@ all of JSON::PP and plus
     $json = $json->add_fallback($case, $cb);
     $json = $json->add_fallback(\@case, $cb);
 
+Add fallback conversions to be applied in the following cases
+
+    a non-ref is found and "allow_nonref" is enabled
+    an unknown is found and "allow_unknown" is enabled
+    a blessed ref is found and "allow_blessed" is enabled
+
 C<$case> should be one of C<'nonref'>, C<'unknonwn'> or C<'blessed'>.
 
 C<$cb> is a subroutine which expects two arguments
@@ -127,3 +203,21 @@ C<$cb> is a subroutine which expects two arguments
         my ($json, $item, $case) = (shift, shift);
         ...
     }
+
+Fallback subroutines are evaluated in list context.
+
+=over 4
+
+=item *
+
+empty list - means try next fallback
+
+=item *
+
+non-empty list - convert first element to JSON
+
+=back
+
+=head1 remove_fallback
+
+    $json = $json->remove_fallback($case, $cb);
